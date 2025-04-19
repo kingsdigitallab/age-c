bfec48e3fcd4fd4c8d85ee07a90de0d57138e5e3
Miguel Vieira <jose.m.vieira@kcl.ac.uk>
2025-04-19 14:11:22 +0100

    feat(frontend): Update header to use full title and enhance styling
    - Changed header to display {config.fullTitle} instead of {config.title}.
    - Added custom styles for the header to improve visual presentation.

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -2,4 +2,15 @@
 	import { config } from '$lib';
 </script>
 
-<h1>Welcome to {config.title}</h1>
+<h1>{config.fullTitle}</h1>
+
+<style>
+	h1 {
+		--pico-color: var(--pico-secondary);
+		--pico-font-family: var(--font-family-gothic);
+		--pico-font-size: clamp(4rem, 20vw, 10rem);
+		--pico-font-weight: 800;
+
+		font-stretch: ultra-condensed;
+	}
+</style>
