f63bb052cff48e69746c92625a94369de766f2a5
Miguel Vieira <jose.m.vieira@kcl.ac.uk>
2025-04-21 12:51:06 +0100

    refactor(home): Update header title and adjust font styles

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -2,14 +2,14 @@
 	import { config } from '$lib';
 </script>
 
-<h1>{config.fullTitle}</h1>
+<h1>{config.siteTitle}</h1>
 
 <style>
 	h1 {
 		--pico-color: var(--pico-secondary);
 		--pico-font-family: var(--font-family-gothic);
-		--pico-font-size: clamp(4rem, 20vw, 10rem);
-		--pico-font-weight: 800;
+		--pico-font-size: clamp(4rem, 25vw, 16rem);
+		--pico-font-weight: 400;
 
 		font-stretch: ultra-condensed;
 	}
