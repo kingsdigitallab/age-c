b126e1c0611dd9daafba2d46987119a02b01b6f5
Miguel Vieira <jose.m.vieira@kcl.ac.uk>
2025-04-22 07:31:19 +0100

    feat(biographies): Add biography detail page
    With data fetching and entry generation.

---
+++ frontend/src/routes/biographies/[slug]/+page.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import type { PageProps } from './$types';
+
+	const { data }: PageProps = $props();
+	const { bio } = data;
+</script>
+
+<article>
+	<hgroup>
+		<h1>{bio.perName}</h1>
+	</hgroup>
+
+	<dl>
+		<dt>Gender</dt>
+		<dd>{bio.perGender}</dd>
+	</dl>
+</article>
