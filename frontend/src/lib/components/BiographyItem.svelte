c6b3a27dd08d17eb9a15d2754e5b328decfbd87d
Miguel Vieira <jose.m.vieira@kcl.ac.uk>
2025-04-25 21:30:55 +0100

    feat(components): Add table components for displaying biographies and films

--- frontend/src/lib/components/BiographyItem.svelte
+++
@@ -1,9 +0,0 @@
-<script lang="ts">
-	import type { Item } from '$lib/types';
-
-	const { item = undefined }: { item: Item | undefined } = $props();
-</script>
-
-{#if item}
-	<strong>{item.perName}</strong>
-{/if}
