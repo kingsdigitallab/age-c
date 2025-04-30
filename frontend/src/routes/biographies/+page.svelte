4ec9c351243f81cf235eab5dd7df15e00631467f
Miguel Vieira <jose.m.vieira@kcl.ac.uk>
2025-04-30 15:49:56 +0100

    refactor(routes): Remove biographies and films routes
    - Deleted the biographies and films routes, including their server and page components.
    - We now have a single entry point for both data types, from the home page.

--- frontend/src/routes/biographies/+page.svelte
+++
@@ -1,6 +0,0 @@
-<script lang="ts">
-	import BiographyItem from '$lib/components/BiographiesTable.svelte';
-	import Search from '$lib/search/Search.svelte';
-</script>
-
-<Search dataSource="biographies" title="Biographies" ItemComponent={BiographyItem} />
--- frontend/src/routes/films/+page.svelte
+++
@@ -1,6 +0,0 @@
-<script lang="ts">
-	import FilmItem from '$lib/components/FilmsTable.svelte';
-	import Search from '$lib/search/Search.svelte';
-</script>
-
-<Search dataSource="films" title="Films" ItemComponent={FilmItem} />
