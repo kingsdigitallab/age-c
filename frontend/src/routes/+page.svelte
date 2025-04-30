d16e4113fe573f4628e1c51102ccaa98258a5e58
Miguel Vieira <jose.m.vieira@kcl.ac.uk>
2025-04-30 15:51:50 +0100

    feat(routes): Add explore search to home page

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,16 +1,26 @@
 <script lang="ts">
 	import { config } from '$lib';
+	import { searchConfig } from '$lib/index';
+	import Search from '$lib/search/Search.svelte';
+
+	const dataSource = $state('corpus');
 </script>
 
-<h1>{config.siteTitle}</h1>
+<section>
+	<h1>{config.siteTitle}</h1>
+</section>
+
+<Search {dataSource} {searchConfig} title="Explore the dataset" />
 
 <style>
 	h1 {
 		--pico-color: var(--pico-secondary);
 		--pico-font-family: var(--font-family-gothic);
-		--pico-font-size: clamp(4rem, 25vw, 16rem);
+		--pico-font-size: clamp(4rem, 20vw, 12rem);
 		--pico-font-weight: 400;
 
 		font-stretch: ultra-condensed;
+		text-align: center;
+		text-wrap: balance;
 	}
 </style>
