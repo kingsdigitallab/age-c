55018220a1d997fb55a2ef9ffd648b00ab22530a
Miguel Vieira <jose.m.vieira@kcl.ac.uk>
2025-05-01 16:02:52 +0100

    feat(landing): Add site description and about link

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -3,32 +3,42 @@
 	import { searchConfig } from '$lib/index';
 	import SearchResultsItems from '$lib/components/SearchResultsItems.svelte';
 	import Search from '$lib/search/Search.svelte';
+	import { base } from '$app/paths';
 
 	const dataSource = $state('corpus');
 </script>
 
-<section>
+<section class="hero">
 	<h1>{config.siteTitle}</h1>
+	<p>{config.siteDescription}</p>
+	<a role="button" href="{base}/about"> About </a>
 </section>
 
-<section>
-	<Search
-		{dataSource}
-		{searchConfig}
-		title="Explore the dataset"
-		SearchResultsItemsComponent={SearchResultsItems}
-	/>
-</section>
+<Search
+	{dataSource}
+	{searchConfig}
+	title="Explore the dataset"
+	SearchResultsItemsComponent={SearchResultsItems}
+/>
 
 <style>
-	h1 {
+	.hero {
+		min-height: calc(100vh - var(--pico-spacing) * 10);
+		text-align: center;
+	}
+
+	.hero h1 {
 		--pico-color: var(--pico-secondary);
 		--pico-font-family: var(--font-family-gothic);
-		--pico-font-size: clamp(4rem, 20vw, 12rem);
+		--pico-font-size: clamp(4rem, 20vw, 10rem);
 		--pico-font-weight: 400;
 
 		font-stretch: ultra-condensed;
 		text-align: center;
 		text-wrap: balance;
+	}
+
+	.hero p {
+		--pico-font-size: clamp(1.5rem, 5vw, 2rem);
 	}
 </style>
