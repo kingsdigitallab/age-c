7497337f34d400dbcb8163cbe5383038cc9fc0dd
Miguel Vieira <jose.m.vieira@kcl.ac.uk>
2025-05-08 12:16:52 +0100

    refactor(search): Replace string literals with constants for worker actions

--- frontend/src/lib/search/worker.ts
+++ frontend/src/lib/search/worker.ts
@@ -1,19 +1,20 @@
 import { searchConfig } from '$lib';
+import { WORKER_STATUS } from './config';
 import { initSearchEngine, search } from './search';
 
 addEventListener('message', async (event) => {
 	const { action, payload } = event.data;
 
-	if (action === 'load') {
+	if (action === WORKER_STATUS.LOAD) {
 		const { basePath, dataSource } = payload;
 		const config = searchConfig[dataSource as keyof typeof searchConfig];
 		const data = await fetch(`${basePath}/api/search/${dataSource}.json`).then((res) => res.json());
 
 		initSearchEngine(dataSource, data, config);
-		postMessage({ action: 'ready' });
-	} else if (action === 'search') {
+		postMessage({ action: WORKER_STATUS.READY });
+	} else if (action === WORKER_STATUS.SEARCH) {
 		const results = search(payload);
 
-		postMessage({ action: 'results', payload: { query: payload.query, results } });
+		postMessage({ action: WORKER_STATUS.RESULTS, payload: { query: payload.query, results } });
 	}
 });
