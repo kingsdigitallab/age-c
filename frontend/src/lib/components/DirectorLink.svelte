3284951eb53ba123c4b1d78cd06def6838751416
Miguel Vieira <jose.m.vieira@kcl.ac.uk>
2025-05-09 15:39:32 +0100

    feat(director): Add DirectorLink component for linking to individual director biographies

---
+++ frontend/src/lib/components/DirectorLink.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import { base } from '$app/paths';
+	import type { Director } from '$lib/types';
+
+	const { director }: { director: Director } = $props();
+</script>
+
+<a href={`${base}/biography/${director.slug}`}
+	>{director.name} ({director.birthYear}–{director.deathYear}, {director.gender})</a
+>
