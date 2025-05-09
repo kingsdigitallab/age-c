df4cfa34379e871db026780fef4b2348f4abb1fd
Miguel Vieira <jose.m.vieira@kcl.ac.uk>
2025-05-09 17:01:19 +0100

    feat(characters): Add CharactersTable component to display character details

---
+++ frontend/src/lib/components/CharactersTable.svelte
@@ -0,0 +1,55 @@
+<script lang="ts">
+	import { config } from '$lib/index';
+	import type { Character } from '$lib/types';
+	import BiographyLink from './BiographyLink.svelte';
+	import FilmLink from './FilmLink.svelte';
+	import FilterLink from './FilterLink.svelte';
+
+	const { characters, linkToFilm = false }: { characters: Character[]; linkToFilm?: boolean } =
+		$props();
+</script>
+
+<table>
+	<thead>
+		<tr>
+			<th>{linkToFilm ? 'Film' : 'Actor'}</th>
+			<th>Role</th>
+			<th>Age</th>
+			<th>Gender</th>
+			<th>Sexuality</th>
+			<th>Origin</th>
+			<th>Class</th>
+			<th>Profession</th>
+			<th>Ability</th>
+			<th>Assisted Mobility</th>
+		</tr>
+	</thead>
+	{#each characters as character}
+		<tr>
+			<td>
+				<strong>
+					{#if linkToFilm}
+						<FilmLink film={character.film} />
+					{:else}
+						<BiographyLink person={character?.person} showAgeGender={false} />
+					{/if}
+				</strong>
+			</td>
+			<td>{character?.role || config.emptyPlaceholder}</td>
+			<td><FilterLink name="characterAge" value={character?.age} /></td>
+			<td><FilterLink name="characterGender" value={character?.gender} /></td>
+			<td><FilterLink name="characterSexuality" value={character?.sexuality} /></td>
+			<td><FilterLink name="characterOrigin" value={character?.origin} /></td>
+			<td><FilterLink name="characterClass" value={character?.class} /></td>
+			<td><FilterLink name="characterProfession" value={character?.profession} /></td>
+			<td><FilterLink name="characterAbility" value={character?.ability} /></td>
+			<td><FilterLink name="assistedMobility" value={character?.assistedMobility} /></td>
+		</tr>
+	{/each}
+</table>
+
+<style>
+	table > * {
+		font-size: 0.8rem;
+	}
+</style>
