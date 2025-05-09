ef57bda62947399cf05ebe719b1afd3ed3b77893
Miguel Vieira <jose.m.vieira@kcl.ac.uk>
2025-05-09 15:39:24 +0100

    feat(biography): Add BiographyLink component for linking to individual biographies

---
+++ frontend/src/lib/components/BiographyLink.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import { base } from '$app/paths';
+	import type { Person } from '$lib/types';
+
+	const { person, showAgeGender = true }: { person: Person; showAgeGender?: boolean } = $props();
+</script>
+
+<a href={`${base}/biography/${person?.slug}`}>
+	{person?.name}
+	{#if showAgeGender}
+		({person?.birthYear}–{person?.deathYear}, {person?.gender})
+	{/if}
+</a>
