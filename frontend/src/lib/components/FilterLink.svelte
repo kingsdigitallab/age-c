4ce10cf961b72502b0558a7e9730b294722d6623
Miguel Vieira <jose.m.vieira@kcl.ac.uk>
2025-05-09 15:39:47 +0100

    feat(filter): Add FilterLink component for dynamic filtering in the application

---
+++ frontend/src/lib/components/FilterLink.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+	import { base } from '$app/paths';
+
+	const { name, value }: { name: string; value: string } = $props();
+</script>
+
+<a href={`${base}/?filters={"${name}"%3A["${value}"]}#skij`}>{value}</a>
