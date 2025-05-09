ae7a8ac69c1bf9b90fe8fb53d0b39b2e1f2a1e72
Miguel Vieira <jose.m.vieira@kcl.ac.uk>
2025-05-09 17:00:45 +0100

    feat(film): Add FilmLink component for linking to individual films

---
+++ frontend/src/lib/components/FilmLink.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import { base } from '$app/paths';
+	import type { Film } from '$lib/types';
+
+	const { film }: { film: Film } = $props();
+</script>
+
+<a href={`${base}/film/${film.slug}`}>
+	{film.title.native}
+	{#if film.title.english}
+		/ {film.title.english}
+	{/if}
+	({film.release.year})
+</a>
