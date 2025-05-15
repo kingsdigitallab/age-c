efa42d5202159e40e22d1ce55903c967250d3f83
Miguel Vieira <jose.m.vieira@kcl.ac.uk>
2025-05-15 07:18:29 +0100

    feat(search): Add option to reload the search engine

--- frontend/src/lib/search/worker.ts
+++ frontend/src/lib/search/worker.ts
@@ -1,20 +1,31 @@
-import { searchConfig } from '$lib';
 import { WORKER_STATUS } from './config';
-import { initSearchEngine, search } from './search';
+import { initSearchEngine, reloadSearchEngine, search } from './search';
 
 addEventListener('message', async (event) => {
 	const { action, payload } = event.data;
 
-	if (action === WORKER_STATUS.LOAD) {
-		const { basePath, dataSource } = payload;
-		const config = searchConfig[dataSource as keyof typeof searchConfig];
-		const data = await fetch(`${basePath}/api/search/${dataSource}.json`).then((res) => res.json());
+	switch (action) {
+		case WORKER_STATUS.LOAD: {
+			postMessage({ action: WORKER_STATUS.LOAD });
 
-		initSearchEngine(dataSource, data, config);
-		postMessage({ action: WORKER_STATUS.READY });
-	} else if (action === WORKER_STATUS.SEARCH) {
-		const results = search(payload);
+			const { basePath, dataSource, config, reload = false } = payload;
+			const data = await fetch(`${basePath}/api/search/${dataSource}.json`).then((res) =>
+				res.json()
+			);
 
-		postMessage({ action: WORKER_STATUS.RESULTS, payload: { query: payload.query, results } });
+			if (reload) {
+				reloadSearchEngine(dataSource, data, config);
+			} else {
+				initSearchEngine(dataSource, data, config);
+			}
+
+			postMessage({ action: WORKER_STATUS.READY });
+			break;
+		}
+		case WORKER_STATUS.SEARCH: {
+			const results = search(payload);
+			postMessage({ action: WORKER_STATUS.RESULTS, payload: { query: payload.query, results } });
+			break;
+		}
 	}
 });
