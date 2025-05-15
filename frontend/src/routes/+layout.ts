5f1bd492be1e5a5832729ea015129c196077954f
Miguel Vieira <jose.m.vieira@kcl.ac.uk>
2025-05-15 07:20:36 +0100

    refactor(layout): Update search worker initialisation to use search configuration

--- frontend/src/routes/+layout.ts
+++ frontend/src/routes/+layout.ts
@@ -1,12 +1,13 @@
 import { browser } from '$app/environment';
 import { base } from '$app/paths';
+import { searchConfig } from '$lib';
 import { initWorker } from '$lib/search/worker.svelte';
 import type { LayoutLoad } from './$types';
 
 export const prerender = true;
 
 export const load: LayoutLoad = async () => {
-	const searchWorker = browser ? initWorker(base, 'corpus') : undefined;
+	const searchWorker = browser ? initWorker(base, 'corpus', searchConfig.corpus) : undefined;
 
 	return {
 		searchWorker
