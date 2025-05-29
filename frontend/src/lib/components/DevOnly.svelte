ab1064888154fa3123f3b6ec2525053c243454ad
Miguel Vieira <jose.m.vieira@kcl.ac.uk>
2025-05-29 09:15:46 +0100

    feat(DevOnly): Add DevOnly component to conditionally render content in a dev environment

---
+++ frontend/src/lib/components/DevOnly.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	import { dev } from '$app/environment';
+	import type { Snippet } from 'svelte';
+
+	const { children }: { children: Snippet } = $props();
+</script>
+
+{#if dev}
+	<code>
+		{@render children?.()}
+	</code>
+{/if}
+
+<style>
+	code {
+		margin-block: 0.25rem;
+	}
+</style>
