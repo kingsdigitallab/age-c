c0a53a92afe8e284240444fe27fe0b51354c48f7
Miguel Vieira <jose.m.vieira@kcl.ac.uk>
2025-06-02 18:06:30 +0100

    refactor(biography): Update biography data handling and component structure to support new roles and character data

--- frontend/src/routes/biography/[slug]/+page.server.ts
+++ frontend/src/routes/biography/[slug]/+page.server.ts
@@ -1,13 +1,13 @@
 import { dev } from '$app/environment';
 import { base } from '$app/paths';
 import { getBiographyData, getData } from '$lib/data';
-import type { Item } from '$lib/types';
+import type { Item, Person } from '$lib/types';
 import { error } from '@sveltejs/kit';
 import type { EntryGenerator, PageServerLoad } from './$types';
 
 export const load: PageServerLoad = async ({ fetch, params: { slug } }) => {
 	try {
-		let bio: Item;
+		let bio: Person;
 
 		if (dev) {
 			const response = await fetch(`${base}/api/biographies/${slug}.json`);
@@ -18,7 +18,7 @@ export const load: PageServerLoad = async ({ fetch, params: { slug } }) => {
 
 		return {
 			title: bio.name,
-			bio: bio as Item
+			bio: bio as Person
 		};
 	} catch (e) {
 		error(404, `Biography not found: ${e instanceof Error ? e.message : e}`);
--- frontend/src/routes/biography/[slug]/+page.svelte
+++ frontend/src/routes/biography/[slug]/+page.svelte
@@ -1,15 +1,19 @@
 <script lang="ts">
 	import FilterLink from '$lib/components/FilterLink.svelte';
 	import CharactersTable from '$lib/components/CharactersTable.svelte';
-	import FilmLink from '$lib/components/FilmLink.svelte';
+	import RolesTable from '$lib/components/PersonRolesTable.svelte';
 	import type { PageProps } from './$types';
 
 	const { data }: PageProps = $props();
 	const { bio } = data;
 
-	const directors = $derived(bio.director);
 	const characters = $derived(
-		bio.character?.sort((a, b) =>
+		bio.characters?.sort((a, b) =>
+			(a.film?.title?.native ?? '').localeCompare(b.film?.title?.native ?? '')
+		)
+	);
+	const roles = $derived(
+		bio.roles?.sort((a, b) =>
 			(a.film?.title?.native ?? '').localeCompare(b.film?.title?.native ?? '')
 		)
 	);
@@ -19,22 +23,20 @@
 	<hgroup>
 		<h1>{bio.name}</h1>
 		<p class="layout-inline">
-			<span>{bio.birthYear}–{bio.deathYear}</span>
+			{#if bio.birthYear || bio.deathYear}
+				<span>{bio.birthYear || ''}–{bio.deathYear || ''}</span>
+			{/if}
 			<FilterLink name="gender" value={bio.gender} />
 			<FilterLink name="nationality" value={bio.nationality} />
 		</p>
 	</hgroup>
 
-	{#if directors}
-		<h2>Director</h2>
-		<ul>
-			{#each directors as director}
-				<li><FilmLink film={director} /></li>
-			{/each}
-		</ul>
+	{#if roles && roles.length > 0}
+		<h2>Roles</h2>
+		<RolesTable {roles} />
 	{/if}
 
-	{#if characters}
+	{#if characters && characters.length > 0}
 		<h2>Characters</h2>
 		<CharactersTable {characters} linkToFilm />
 	{/if}
