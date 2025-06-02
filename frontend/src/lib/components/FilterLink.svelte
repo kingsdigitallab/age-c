2d58b490c91e55435f9d23f347c6489c19c9b159
Miguel Vieira <jose.m.vieira@kcl.ac.uk>
2025-06-02 18:09:54 +0100

    refactor(filter-link): Update type definitions and conditional rendering logic

--- frontend/src/lib/components/FilterLink.svelte
+++ frontend/src/lib/components/FilterLink.svelte
@@ -2,13 +2,23 @@
 	import { base } from '$app/paths';
 	import type { Snippet } from 'svelte';
 
-	const { name, value, children }: { name: string; value: string; children: Snippet } = $props();
+	const {
+		name,
+		value,
+		children
+	}: { name: string; value: string | number | undefined; children?: Snippet } = $props();
 </script>
 
-<a href="{base}/?filters={JSON.stringify({ [name]: [value] })}&#skij">
-	{#if children}
-		{@render children()}
-	{:else}
-		{value}
-	{/if}
-</a>
+{#if value}
+	<a href="{base}/?filters={JSON.stringify({ [name]: [value] })}&#skij">
+		{#if children}
+			{@render children()}
+		{:else}
+			{value}
+		{/if}
+	</a>
+{:else if children}
+	{@render children()}
+{:else}
+	<mark>{value}</mark>
+{/if}
