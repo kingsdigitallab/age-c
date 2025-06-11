691aaf77bbd53ebd590e68825e0bd1dc6245b8a2
jmiguelv <jmiguelv@users.noreply.github.com>
2025-06-11 15:43:20 +0000

    deploy: 58658292df3c66b896594b9e2814d094e4fe85b3

---
+++ _app/immutable/chunks/FZWBWeuQ.js
@@ -0,0 +1,1 @@
+var s;const e=((s=globalThis.__sveltekit_fny1a2)==null?void 0:s.base)??"/age-c";var a;const t=((a=globalThis.__sveltekit_fny1a2)==null?void 0:a.assets)??e;export{t as a,e as b};
---
+++ _app/immutable/entry/start.D0c-F5mw.js
@@ -0,0 +1,1 @@
+import{l as o,b as r}from"../chunks/BC6aVbhi.js";export{o as load_css,r as start};
