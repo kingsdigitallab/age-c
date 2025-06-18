b4315c969fb30ac49ba945439ffd558babd51045
Miguel Vieira <jose.m.vieira@kcl.ac.uk>
2025-06-18 07:07:59 +0100

    feat(search): Add support for combined facets
    - Updated initSearchEngine to handle skijCombineFilters option.
    - Added expandConfigWithCombinations function to dynamically add combined facets  to the search configuration.

--- frontend/src/lib/search/search.ts
+++ frontend/src/lib/search/search.ts
@@ -1,20 +1,39 @@
 import type { Item } from '$lib/types';
 // @ts-expect-error Could not find a declaration file for module 'itemsjs'
 import itemsjs from 'itemsjs';
-import type { SearchParams, SearchEngineKey } from './types';
+import type { SearchParams, SearchEngineKey, SearchConfig } from './types';
 
 const searchEngines = {} as Record<SearchEngineKey, itemsjs>;
 
-export function initSearchEngine(
-	dataSource: SearchEngineKey,
-	data: Item[],
-	config: Record<string, unknown>
-) {
+export function initSearchEngine(dataSource: SearchEngineKey, data: Item[], config: SearchConfig) {
 	if (searchEngines[dataSource]) {
 		return;
 	}
 
-	searchEngines[dataSource] = itemsjs(data, config);
+	if (config.skijCombineFilters) {
+		const configWithCombinations = expandConfigWithCombinations(config);
+		console.log('configWithCombinations', configWithCombinations);
+		searchEngines[dataSource] = itemsjs(data, configWithCombinations);
+	} else {
+		searchEngines[dataSource] = itemsjs(data, config);
+	}
+}
+
+function expandConfigWithCombinations(config: SearchConfig) {
+	const expandedConfig = { ...config };
+
+	for (const [facet, facetConfig] of Object.entries(config.aggregations)) {
+		if (facetConfig.skijCombineWith) {
+			for (const combineWith of facetConfig.skijCombineWith) {
+				const [key, _] = Object.entries(combineWith)[0];
+				expandedConfig.aggregations[`${facet}:::${key}`] = {
+					title: `${facetConfig.title} and ${key}`
+				};
+			}
+		}
+	}
+
+	return expandedConfig;
 }
 
 export function reloadSearchEngine(
