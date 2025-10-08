73c1b61b1167d1dedccacd792cbed9e3a0fbb7ef
jmiguelv <jmiguelv@users.noreply.github.com>
2025-10-08 14:16:23 +0000

    deploy: b0073c25361d52167f482ab372065baf737599b0

---
+++ _app/immutable/chunks/CWE6-ej9.js
@@ -0,0 +1,1 @@
+var s;const a=((s=globalThis.__sveltekit_tkfcs1)==null?void 0:s.base)??"/age-c";var t;const e=((t=globalThis.__sveltekit_tkfcs1)==null?void 0:t.assets)??a;export{e as a,a as b};
---
+++ _app/immutable/entry/start.CrKscEy1.js
@@ -0,0 +1,1 @@
+import{l as o,b as r}from"../chunks/-_22Vht9.js";export{o as load_css,r as start};
