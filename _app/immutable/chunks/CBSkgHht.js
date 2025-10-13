e5671551b526f976d922052ac2dc7f9dad119cb5
jmiguelv <jmiguelv@users.noreply.github.com>
2025-10-13 17:54:20 +0000

    deploy: 4052d080bd03cbeb496eb4f9f4a9f6e3a6a9cce4

---
+++ _app/immutable/chunks/CBSkgHht.js
@@ -0,0 +1,1 @@
+import{s as t,p as r}from"./Dj02w55N.js";const e={get data(){return r.data},get error(){return r.error},get status(){return r.status},get url(){return r.url}};t.updated.check;const s=e;export{s as p};
